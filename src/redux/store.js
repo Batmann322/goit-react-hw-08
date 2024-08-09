1cd68de8c676700402cb7b31d64a89d2c2aef53d
Nazar <n.kopilchak@gmail.com>
2024-08-09 22:29:13 +0300

    done

--- src/redux/store.js
+++ src/redux/store.js
@@ -10,9 +10,9 @@ import {
   REGISTER,
 } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-import authReducer from "./auth/authSlice";
-import contactsReducer from "./contacts/contactsSlice";
-import filtersReducer from "./filters/filtersSlice";
+import authReducer from "./auth/Slice";
+import contactsReducer from "./contacts/Slice";
+import filtersReducer from "./filters/Slice";
 
 const authPersistConfig = {
   key: "auth",
